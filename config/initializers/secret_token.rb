6f01e7737e31d03fd4daf5d884eadab8c1625dc1
Nguyen Vu Nguyen <nvunguyen@gmail.com>
2011-11-01 10:54:12 +0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Orms::Application.config.secret_token = '5e16ae329339ddb9eeaa854fd1d92d906b93c34f555ddfd2bba2678c4ae31e823e8913cc47c1aa1099bc20dd8c4e0fcda14451a8085abe275266449f16439a89'
